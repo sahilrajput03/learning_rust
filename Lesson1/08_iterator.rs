0e101a2e0d6274e59ee921c8916567c3bffedfe3
Sahil Rajput <sahilrajput03@gmail.com>
2022-01-23 21:47:27 +0530

    Fix older example.

--- Lesson1/08_iterator.rs
+++ Lesson1/08_iterator.rs
@@ -4,4 +4,12 @@ fn main() {
 	for a in animals.iter() {
 		println!("The animal name is {}", a);
 	}
+	println!("");
+	//
+	//
+
+	// A BETTER WAY: Coz referencing to the vector gets us the ```iter()``` method. ~Sahil.
+	for a in &animals {
+		println!("The animal name is {}", a);
+	}
 }
